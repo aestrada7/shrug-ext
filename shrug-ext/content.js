c5259b9dbf39bdd89e54264bc4d185f8de47d3f6
aestrada7 <vanhelsing44@gmail.com>
2016-05-26 13:41:55 -0500

    Added keyboard shortcut

--- shrug-ext/content.js
+++ shrug-ext/content.js
@@ -1,6 +1,10 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if(request.message === 'clicked_browser_action') {
-    var currentValue = document.activeElement.value;
-    document.activeElement.value = currentValue + '¯\\_(ツ)_/¯';
+  if(request.message === 'clicked_browser_action' || request.message === 'pressed_hotkey_action') {
+    addShrug();
   }
 });
+
+function addShrug() {
+  var currentValue = document.activeElement.value;
+  document.activeElement.value = currentValue + '¯\\_(ツ)_/¯';
+}
--- shrug-ext/listener.js
+++ shrug-ext/listener.js
@@ -4,3 +4,11 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(activeTab.id, {'message': 'clicked_browser_action'});
   });
 });
+
+chrome.commands.onCommand.addListener(function(command) {
+  chrome.tabs.update({}, function(tab) {
+    if(command === 'pressed_browser_hotkey') {
+      chrome.tabs.sendMessage(tab.id, {'message': 'pressed_hotkey_action'});
+    }
+  });
+});
